20116b1ba6dc85a7264c36ddf23f2be0665087fd
BijanEisapour <bijanprogrammer@gmail.com>
2021-08-01 09:19:17 +0430

    [ADD] basic database connection

--- app.js
+++ app.js
@@ -1,7 +1,47 @@
 const express = require('express');
+const handlerbars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
+require('dotenv').config();
+
+// constants
 const PORT = process.env.PORT || 5000;
+const ROUTES = {
+    USER: require('./server/routes/user'),
+};
 
+// variables
+
+// init app
 const app = express();
+
+// parse middlewares
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// static folder
+app.use(express.static('public'));
+
+// mysql
+const pool = mysql.createPool({
+    connectionLimit: 100,
+    host: process.env['DB_HOST'],
+    user: process.env['DB_USER'],
+    password: process.env['DB_PASS'],
+    database: process.env['DB_NAME'],
+});
+
+pool.getConnection((err, connection) => {
+    if (err) throw err;
+    console.log(`connected to database as ${connection.threadId}`);
+});
+
+// routers
+app.get('', (req, res) => {
+    res.send('Hello, friend!');
+});
+
+app.use('/user', ROUTES.USER);
 
 app.listen(PORT, () => console.log(`listening on port ${PORT} ...`));
---
+++ enums/errors.js
@@ -0,0 +1,5 @@
+module.exports = {
+    USER_ID_REQUIRED: "The user's id is required.",
+    USER_NOT_FOUND: 'Cannot find the user.',
+    SOMETHING_WENT_WRONG: 'Something went wrong.',
+};
